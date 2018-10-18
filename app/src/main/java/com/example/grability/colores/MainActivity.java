1f46f1c41c69d041d83530557128a29c90c14915
AndresTapiero <andrestapiero72@gmail.com>
2018-10-17 22:49:56 -0500

    Ejercicio Finalizado

--- app/src/main/java/com/example/grability/colores/MainActivity.java
+++ app/src/main/java/com/example/grability/colores/MainActivity.java
@@ -2,14 +2,13 @@ package com.example.grability.colores;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.annotation.ColorInt;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    int mA = 0, mB=0, mC=0;
     private ImageView imageV;
     private CheckBox bAmarillo, bAzul, bRojo;
 
@@ -19,44 +18,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         imageV = (ImageView) findViewById(R.id.Ccolor);
         bAmarillo = (CheckBox) findViewById(R.id.checkBoxAmarillo);
-        bAzul = (CheckBox)findViewById(R.id.checkBoxAzul);
-        bRojo = (CheckBox)findViewById(R.id.checkBoxRojo);
+        bAzul = (CheckBox) findViewById(R.id.checkBoxAzul);
+        bRojo = (CheckBox) findViewById(R.id.checkBoxRojo);
     }
 
-    int mA = 0;
-    int mB = 0;
-    int mC=0;
     public void Onclick(View view) {
 
-         switch (view.getId()) {
+        switch (view.getId()) {
 
             case R.id.checkBoxAmarillo:
-               if(bAmarillo.isChecked()){
-                   imageV.setBackgroundColor(Color.YELLOW);
-                   mA++;
-               }else{
-                   imageV.setBackgroundColor(Color.TRANSPARENT);
-                   mA--;
-               }
+                if (bAmarillo.isChecked()) {
+                    imageV.setBackgroundResource(R.color.colorAmarillo);
+                    mA++;
+                } else {
+                    imageV.invalidate();
+                    mA--;
+                }
 
                 break;
 
             case R.id.checkBoxAzul:
-                if(bAzul.isChecked()){
-                    imageV.setBackgroundColor(Color.BLUE);
+                if (bAzul.isChecked()) {
+                    imageV.setBackgroundResource(R.color.colorAzul);
                     mB++;
-                }else{
-                    imageV.setBackgroundColor(Color.WHITE);
+                } else {
+                    imageV.invalidate();
                     mB--;
                 }
                 break;
 
             case R.id.checkBoxRojo:
-                if(bRojo.isChecked()){
-                    imageV.setBackgroundColor(Color.RED);
+                if (bRojo.isChecked()) {
+                    imageV.setBackgroundResource(R.color.colorRojo);
                     mC++;
-                }else{
-                    imageV.setBackgroundColor(Color.TRANSPARENT);
+                } else {
+                    imageV.invalidate();
                     mC--;
                 }
 
@@ -65,14 +61,21 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
 
-        if (mA==0 && mB==0 && mC==0){
+        if (mA == 0 && mB == 0 && mC == 0) {
             imageV.setBackgroundColor(Color.TRANSPARENT);
-        }else if(mA==1 && mB==1){
+        } else if (mA == 1 && mB == 1) {
             imageV.setBackgroundResource(R.color.colorGreen);
-        }else if(mA==1 && mC==1){
+        } else if (mA == 1 && mC == 1) {
             imageV.setBackgroundResource(R.color.colorOrange);
-        }else if (mB==1&& mC==1){
+        } else if (mB == 1 && mC == 1) {
             imageV.setBackgroundResource(R.color.colorMagenta);
+        } else if (mA == 1) {
+            imageV.setBackgroundResource(R.color.colorAmarillo);
+        } else if (mB == 1) {
+            imageV.setBackgroundResource(R.color.colorAzul);
+        } else if (mC == 1) {
+            imageV.setBackgroundResource(R.color.colorRojo);
+
         }
     }
 }
